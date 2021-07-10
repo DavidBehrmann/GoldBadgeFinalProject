76101e4e596292aff43d7a2dba872f7b4a9d92d9
David Behrmann <davidbehrmanntx@gmail.com>
2021-07-10 13:54:38 -0400

    created Poco class

--- CafeREPO/CafeRepo.cs
+++ CafeREPO/CafeRepo.cs
@@ -8,38 +8,28 @@ namespace CafeREPO
 {
 
 
-    //Methods
+    
     public class CafeRepo
     {
         //Create menu items
-
-        //Read list of menue items
-
-        //Delete menu items
-
-
-    }
-
-    //POCO
-    public class MenuPoco
-    {
-        public int MealNumber { get; set; }
-        public string MealName { get; set; }
-        public string Description { get; set; }
-        public string Ingredients { get; set; }
-        public double Price { get; set; }
-
-        public MenuPoco() { }
-
-        public MenuPoco(int mealNumber, string mealName, string description, string ingredients, double price)
+        public void CreateMenuItem()
         {
-            MealNumber = mealNumber;
-            MealName = mealName;
-            Description = description;
-            Ingredients = ingredients;
-            Price = price;
+
         }
+        //Read list of menue items
+        public void ReadListOfMenuItems()
+        {
+
+        }
+        //Delete menu items
+        public void RemoveItemsFromMenu()
+        {
+
+        }
+
     }
+
+    
 
 
 
