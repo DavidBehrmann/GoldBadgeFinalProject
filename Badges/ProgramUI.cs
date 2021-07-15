54859b715af5bab6d2258cebeec10520ef1a1172
David Behrmann <davidbehrmanntx@gmail.com>
2021-07-14 21:13:15 -0400

    added doorList txt file

--- Badges/ProgramUI.cs
+++ Badges/ProgramUI.cs
@@ -8,5 +8,16 @@ namespace Badges
 {
     class ProgramUI
     {
+
+        public void Run()
+        {
+            string path = @"C: \Users\david\Desktop\Software Development Course\ElevenFiftyProjects\GoldBadgeFinalProject\Cafe\Badges\DoorList.txt";
+            string text = System.IO.File.ReadAllText(path);
+            List<string> doorList = text.Split(',').ToList();
+
+
+        }
+
+
     }
 }
