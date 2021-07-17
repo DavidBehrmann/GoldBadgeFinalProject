fd8fe4f41be1792e236455af7e9208740963994a
David Behrmann <davidbehrmanntx@gmail.com>
2021-07-17 17:00:59 -0400

    scaffolded out Outings Challenge

--- OutingsREPO/Outings.cs
+++ OutingsREPO/Outings.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace OutingsREPO
 {
+    public enum EventType
+    {
+        Golf,
+        Bowling,
+        AmusementPark,
+        Concert
+    }
     class Outings
     {
+        public Outings() { }
+        public Outings(EventType eventType, int numOfPeopleAttended, DateTime date, double totalCost, double totalCostPerPerson)
+        {
+            EventType = eventType;
+            NumOfPeopleAttended = numOfPeopleAttended;
+            Date = date;
+            TotalCost = totalCost;
+            TotalCostPerPerson = totalCostPerPerson;
+        }
+
+        public EventType EventType { get; set; }
+        public int NumOfPeopleAttended { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalCost { get; set; }
+        public double TotalCostPerPerson { get; set; }
     }
 }
